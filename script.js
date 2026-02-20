1b85f0e29698759cb4f41f7baeaeb87f407b2cfd
prateekSharingan <pk.4081988@gmail.com>
2026-02-20 10:31:37 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello, world!")
+var a = 100;
+var b = 13;
+var lineBreak = '<br>';
+
+document.write('a + b + c = ');
+var result = a + b + c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+
+document.write('a - b - c = ');
+var result = a - b - c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+document.write('a * b * c = ');
+var result = a * b * c;
+
+document.write(result);
+document.write(lineBreak);
+
+
+
+
+document.write('a / b / c = ');
+var result = a / b / c;
+
+document.write(result);
+document.write(lineBreak);
