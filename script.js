35f6de76a3863728a3d93da2687b575b36abb830
prateekSharingan <pk.4081988@gmail.com>
2026-02-27 09:47:21 +0530

    function1

--- script.js
+++ script.js
@@ -1,74 +1,145 @@
-// var a = 10;
-// var b = 20;
+// // // var a = 10;
+// // // var b = 20;
 
-// var linebreak = "<br>";
+// // // var linebreak = "<br>";
 
-// document.write("(a == b) =>");
-// result = (a == b);
-// document.write(result);
-// document.write(linebreak);
-
-
-// document.write("(a < b) =>");
-// result = (a < b);
-// document.write(result);
-// document.write(linebreak);
+// // // document.write("(a == b) =>");
+// // // result = (a == b);
+// // // document.write(result);
+// // // document.write(linebreak);
 
 
-
-// document.write("(a > b) =>");
-// result = (a > b);
-// document.write(result);
-// document.write(linebreak);
+// // // document.write("(a < b) =>");
+// // // result = (a < b);
+// // // document.write(result);
+// // // document.write(linebreak);
 
 
 
-// document.write("(a >= b) =>");
-// result = (a >= b);
-// document.write(result);
-// document.write(linebreak);
+// // // document.write("(a > b) =>");
+// // // result = (a > b);
+// // // document.write(result);
+// // // document.write(linebreak);
 
 
 
-// document.write("(a <= b) =>");
-// result = (a <= b);
-// document.write(result);
-// document.write(linebreak);
+// // // document.write("(a >= b) =>");
+// // // result = (a >= b);
+// // // document.write(result);
+// // // document.write(linebreak);
+
+
+
+// // // document.write("(a <= b) =>");
+// // // result = (a <= b);
+// // // document.write(result);
+// // // document.write(linebreak);
 
 
 
 
-// document.write("(a != b) =>");
-// result = (a != b);
-// document.write(result);
-// document.write(linebreak);
+// // // document.write("(a != b) =>");
+// // // result = (a != b);
+// // // document.write(result);
+// // // document.write(linebreak);
 
 
-// // var count;
+// // // // var count;
 
-// // for(count = 0;count < 10; count ++){
+// // // // for(count = 0;count < 10; count ++){
 
-// //   document.write("current count :" + count);
+// // // //   document.write("current count :" + count);
 
-// //   document.write("<br>");
+// // // //   document.write("<br>");
+// // // // }
+
+// // // // document.write("<br>");
+
+// // // // document.write(count);
+
+
+
+// // // const person = {
+// // //   fname:"Ajay",
+// // //   lname:"Singh",
+// // //   age:45
+
+// // // };
+
+// // // for(let x in person){
+
+
+// // //   console.log("Person Details: " + x + ": " +person[x]);
+// // // }
+
+// // // var count = 0;
+
+// // // while(count < 10){
+
+// // //   console.log(count);
+// // //   count++;
+// // // }
+
+
+// // // var age = 10;
+
+// // // if(age >= 18){
+
+// // //   console.log("You are eligible to vote");
+
+// // // }
+
+// // // else{
+
+// // //   console.log("You are minor");
+
+// // // }
+
+
+
+// // var grade = 'E';
+
+// // switch(grade){
+
+// //   case 'A':
+// //     console.log("Good Job");
+// //     break;
+
+// //     case 'B':
+// //     console.log("Pretty Good");
+// //     break;
+
+// //     case 'C':
+// //     console.log("Passed");
+// //     break;
+
+// //     case 'D':
+// //     console.log("Not so good");
+// //     break;
+
+// //     case 'E':
+// //     console.log("Failed");
+// //     break;
+
+// //     default:
+// //       console.log("Unknown Grade");
+
+
+
+    
 // // }
 
-// // document.write("<br>");
-
-// // document.write(count);
 
 
-
-const person = {
-  fname:"Ajay",
-  lname:"Singh",
-  age:45
-
-};
-
-for(let x in person){
+// function sayHello(name,age)
+// {
+//   document.write(name + " is " + age + " years old." );
+// }
 
 
-  console.log("Person Details: " + x + ": " +person[x]);
+var res = mul(12,30);
+
+function mul(x, y){
+  return x * y;
 }
-
+console.log(res);
