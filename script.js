49c89fa7d8723c2ba4bc0dee682861bc290be874
prateekSharingan <pk.4081988@gmail.com>
2026-03-09 15:52:22 +0530

    DOM

--- script.js
+++ script.js
@@ -1,196 +1,239 @@
-// // // // // var a = 10;
-// // // // // var b = 20;
+// // // // // // // // // // var a = 10;
+// // // // // // // // // // var b = 20;
 
-// // // // // var linebreak = "<br>";
+// // // // // // // // // // var linebreak = "<br>";
 
-// // // // // document.write("(a == b) =>");
-// // // // // result = (a == b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
-
-
-// // // // // document.write("(a < b) =>");
-// // // // // result = (a < b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
+// // // // // // // // // // document.write("(a == b) =>");
+// // // // // // // // // // result = (a == b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
 
 
-
-// // // // // document.write("(a > b) =>");
-// // // // // result = (a > b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
+// // // // // // // // // // document.write("(a < b) =>");
+// // // // // // // // // // result = (a < b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
 
 
 
-// // // // // document.write("(a >= b) =>");
-// // // // // result = (a >= b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
+// // // // // // // // // // document.write("(a > b) =>");
+// // // // // // // // // // result = (a > b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
 
 
 
-// // // // // document.write("(a <= b) =>");
-// // // // // result = (a <= b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
+// // // // // // // // // // document.write("(a >= b) =>");
+// // // // // // // // // // result = (a >= b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
+
+
+
+// // // // // // // // // // document.write("(a <= b) =>");
+// // // // // // // // // // result = (a <= b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
 
 
 
 
-// // // // // document.write("(a != b) =>");
-// // // // // result = (a != b);
-// // // // // document.write(result);
-// // // // // document.write(linebreak);
+// // // // // // // // // // document.write("(a != b) =>");
+// // // // // // // // // // result = (a != b);
+// // // // // // // // // // document.write(result);
+// // // // // // // // // // document.write(linebreak);
 
 
-// // // // // // var count;
+// // // // // // // // // // // var count;
 
-// // // // // // for(count = 0;count < 10; count ++){
+// // // // // // // // // // // for(count = 0;count < 10; count ++){
 
-// // // // // //   document.write("current count :" + count);
+// // // // // // // // // // //   document.write("current count :" + count);
 
-// // // // // //   document.write("<br>");
-// // // // // // }
+// // // // // // // // // // //   document.write("<br>");
+// // // // // // // // // // // }
 
-// // // // // // document.write("<br>");
+// // // // // // // // // // // document.write("<br>");
 
-// // // // // // document.write(count);
-
-
-
-// // // // // const person = {
-// // // // //   fname:"Ajay",
-// // // // //   lname:"Singh",
-// // // // //   age:45
-
-// // // // // };
-
-// // // // // for(let x in person){
-
-
-// // // // //   console.log("Person Details: " + x + ": " +person[x]);
-// // // // // }
-
-// // // // // var count = 0;
-
-// // // // // while(count < 10){
-
-// // // // //   console.log(count);
-// // // // //   count++;
-// // // // // }
-
-
-// // // // // var age = 10;
-
-// // // // // if(age >= 18){
-
-// // // // //   console.log("You are eligible to vote");
-
-// // // // // }
-
-// // // // // else{
-
-// // // // //   console.log("You are minor");
-
-// // // // // }
+// // // // // // // // // // // document.write(count);
 
 
 
-// // // // var grade = 'E';
+// // // // // // // // // // const person = {
+// // // // // // // // // //   fname:"Ajay",
+// // // // // // // // // //   lname:"Singh",
+// // // // // // // // // //   age:45
 
-// // // // switch(grade){
+// // // // // // // // // // };
 
-// // // //   case 'A':
-// // // //     console.log("Good Job");
-// // // //     break;
+// // // // // // // // // // for(let x in person){
 
-// // // //     case 'B':
-// // // //     console.log("Pretty Good");
-// // // //     break;
 
-// // // //     case 'C':
-// // // //     console.log("Passed");
-// // // //     break;
+// // // // // // // // // //   console.log("Person Details: " + x + ": " +person[x]);
+// // // // // // // // // // }
 
-// // // //     case 'D':
-// // // //     console.log("Not so good");
-// // // //     break;
+// // // // // // // // // // var count = 0;
 
-// // // //     case 'E':
-// // // //     console.log("Failed");
-// // // //     break;
+// // // // // // // // // // while(count < 10){
 
-// // // //     default:
-// // // //       console.log("Unknown Grade");
+// // // // // // // // // //   console.log(count);
+// // // // // // // // // //   count++;
+// // // // // // // // // // }
+
+
+// // // // // // // // // // var age = 10;
+
+// // // // // // // // // // if(age >= 18){
+
+// // // // // // // // // //   console.log("You are eligible to vote");
+
+// // // // // // // // // // }
+
+// // // // // // // // // // else{
+
+// // // // // // // // // //   console.log("You are minor");
+
+// // // // // // // // // // }
+
+
+
+// // // // // // // // // var grade = 'E';
+
+// // // // // // // // // switch(grade){
+
+// // // // // // // // //   case 'A':
+// // // // // // // // //     console.log("Good Job");
+// // // // // // // // //     break;
+
+// // // // // // // // //     case 'B':
+// // // // // // // // //     console.log("Pretty Good");
+// // // // // // // // //     break;
+
+// // // // // // // // //     case 'C':
+// // // // // // // // //     console.log("Passed");
+// // // // // // // // //     break;
+
+// // // // // // // // //     case 'D':
+// // // // // // // // //     console.log("Not so good");
+// // // // // // // // //     break;
+
+// // // // // // // // //     case 'E':
+// // // // // // // // //     console.log("Failed");
+// // // // // // // // //     break;
+
+// // // // // // // // //     default:
+// // // // // // // // //       console.log("Unknown Grade");
 
 
 
     
-// // // // }
+// // // // // // // // // }
 
 
 
-// // // function sayHello(name,age)
-// // // {
-// // //   document.write(name + " is " + age + " years old." );
-// // // }
+// // // // // // // // function sayHello(name,age)
+// // // // // // // // {
+// // // // // // // //   document.write(name + " is " + age + " years old." );
+// // // // // // // // }
 
 
-// // // var res = mul(12,30);
+// // // // // // // // var res = mul(12,30);
 
-// // // function mul(x, y){
-// // //   return x * y;
-// // // }
-// // // console.log(res);
+// // // // // // // // function mul(x, y){
+// // // // // // // //   return x * y;
+// // // // // // // // }
+// // // // // // // // console.log(res);
 
-// // var add = (a, b) =>{
-// //   console.log(a + b);
-// // }
+// // // // // // // var add = (a, b) =>{
+// // // // // // //   console.log(a + b);
+// // // // // // // }
 
-// // add(20,30);
+// // // // // // // add(20,30);
 
-// // var add2 = (a, b) =>
-// //   console.log(a + b);
-
-
-// // add2(100,30);
+// // // // // // // var add2 = (a, b) =>
+// // // // // // //   console.log(a + b);
 
 
-
-// var person = {
-//   name:"John",
-//   age:30,
-//   isMarried:true,
-
-//   address :{
-//     street : "Vinay Nagar",
-//     flatNo:201
-//   }
-// };
-
-
-// console.log(person.name);
-// console.log(person.age);
-// console.log(person.isMarried);
-// console.log(person.address);
+// // // // // // // add2(100,30);
 
 
 
-var person2 = new Object();
+// // // // // // var person = {
+// // // // // //   name:"John",
+// // // // // //   age:30,
+// // // // // //   isMarried:true,
 
-person2.name = "prateek";
-
-person2.age = 32;
-
-person2.isMarried = true;
-
-person2.address = {};
-
-person2.address.street = "nagar road";
-
-person2.address.flatNo = 33;
+// // // // // //   address :{
+// // // // // //     street : "Vinay Nagar",
+// // // // // //     flatNo:201
+// // // // // //   }
+// // // // // // };
 
 
-console.log(person);
+// // // // // // console.log(person.name);
+// // // // // // console.log(person.age);
+// // // // // // console.log(person.isMarried);
+// // // // // // console.log(person.address);
 
+
+
+// // // // // var person2 = new Object();
+
+// // // // // person2.name = "prateek";
+
+// // // // // person2.age = 32;
+
+// // // // // person2.isMarried = true;
+
+// // // // // person2.address = {};
+
+// // // // // person2.address.street = "nagar road";
+
+// // // // // person2.address.flatNo = 33;
+
+
+// // // // // console.log(person);
+
+// // // // var myInfo = new Array();
+
+// // // // console.log(myInfo);
+
+// // // // var info1 = Array();
+
+// // // // console.log(info1);
+
+
+
+// // // console.log(String("abc"));
+
+// // // console.log(Date());
+
+
+// // // let arr = [];
+
+// // // arr.teacherNAme = "JOhn mAc";
+
+// // // console.log(arr);
+
+
+// // // let trainerInfo = ["Jitendra", 20, "Ghaziabad", {isMarried: true}];
+
+
+// // // console.log(trainerInfo);
+
+
+// // // console.log(trainerInfo[trainerInfo.length-1].isMarried);
+
+
+
+// // const result = document.getElementById("para");
+
+// // console.log(result);
+
+// // result.innerHTML = "mySelf";
+
+// let test = document.getElementsByClassName("classs");
+
+// test.innerHTML = "hii";
+
+// console.log(test);
